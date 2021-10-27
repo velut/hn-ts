bf33589023631f60fdae4b000c8a2b08c7a78ef3
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2021-10-27 20:54:38 +0200

    test: add tests for different item types

--- test/get-item-by-id.test.ts
+++ test/get-item-by-id.test.ts
@@ -21,24 +21,83 @@ describe("getItemById", () => {
   });
 
   it("returns an existing story item", async () => {
-    expect.assertions(4);
+    expect.assertions(5);
 
     const item = await getItemById({ id: 27107832 });
 
     expect(item).toBeDefined();
     expect(item!.id).toEqual(27107832);
     expect(item!.author).toEqual("velut");
+    expect(item!.type).toEqual("story");
     expect(item).toMatchSnapshot();
   });
 
   it("returns an existing comment item", async () => {
-    expect.assertions(4);
+    expect.assertions(5);
 
     const item = await getItemById({ id: 27121843 });
 
     expect(item).toBeDefined();
     expect(item!.id).toEqual(27121843);
     expect(item!.author).toEqual("velut");
+    expect(item!.type).toEqual("comment");
+    expect(item).toMatchSnapshot();
+  });
+
+  it("returns an existing 'ask hn' story item", async () => {
+    expect.assertions(5);
+
+    const item = await getItemById({ id: 121003 });
+
+    expect(item).toBeDefined();
+    expect(item!.id).toEqual(121003);
+    expect(item!.type).toEqual("story");
+    expect(item!.title!.startsWith("Ask HN")).toEqual(true);
+    expect(item).toMatchSnapshot();
+  });
+
+  it("returns an existing 'show hn' story item", async () => {
+    expect.assertions(5);
+
+    const item = await getItemById({ id: 25496976 });
+
+    expect(item).toBeDefined();
+    expect(item!.id).toEqual(25496976);
+    expect(item!.type).toEqual("story");
+    expect(item!.title!.startsWith("Show HN")).toEqual(true);
+    expect(item).toMatchSnapshot();
+  });
+
+  it("returns an existing job item", async () => {
+    expect.assertions(4);
+
+    const item = await getItemById({ id: 192327 });
+
+    expect(item).toBeDefined();
+    expect(item!.id).toEqual(192327);
+    expect(item!.type).toEqual("job");
+    expect(item).toMatchSnapshot();
+  });
+
+  it("returns an existing poll item", async () => {
+    expect.assertions(4);
+
+    const item = await getItemById({ id: 126809 });
+
+    expect(item).toBeDefined();
+    expect(item!.id).toEqual(126809);
+    expect(item!.type).toEqual("poll");
+    expect(item).toMatchSnapshot();
+  });
+
+  it("returns an existing poll option item", async () => {
+    expect.assertions(4);
+
+    const item = await getItemById({ id: 160705 });
+
+    expect(item).toBeDefined();
+    expect(item!.id).toEqual(160705);
+    expect(item!.type).toEqual("pollopt");
     expect(item).toMatchSnapshot();
   });
 
